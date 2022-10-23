dd4e3e166b8b4c8e4e84768693c7907cb50c496c
Ricky Tran <diabloxtran@gmail.com>
2022-10-23 14:24:09 -0400

    added cover photos for lists and new bookmarks to lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@ movies = JSON.parse(movies_serialized)
 movie_results = movies['results']
 
 movie_results.each do |movie_result|
-  Movie.create!(title: movie_result.original_title, overview: movie_result.overview, poster_url: "https://image.tmdb.org/t/p/w500#{movie_result.poster_path}", rating: movie_result.vote_average)
+  Movie.create!(title: movie_result['title'], overview: movie_result['overview'], poster_url: "https://image.tmdb.org/t/p/w500#{movie_result['poster_path']}", rating: movie_result['vote_average'])
 end
